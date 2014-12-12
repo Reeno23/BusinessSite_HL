38bddbeffc4b119613f419bbde5a498bca87969c
Chris Hume <Reeno2323@gmail.com>
2014-12-12 14:52:38 -0500

    default site commit

---
+++ HLSite/HLSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HLSite.Startup))]
+namespace HLSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
